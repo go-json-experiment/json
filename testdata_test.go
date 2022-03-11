cf8af6dbfd5403c4d1d869dd300f43a33c33fd1c
Joe Tsai <joetsai@digital-static.net>
2022-03-11 13:55:17 -0800

    Add concrete type for the StringEscaped and StringUnicode benchmarks (#141)

--- testdata_test.go
+++ testdata_test.go
@@ -74,6 +74,10 @@ func jsonTestdata() []jsonTestdataEntry {
 			switch name {
 			case "GolangSource":
 				newFn = func() interface{} { return new(golangRoot) }
+			case "StringEscaped":
+				newFn = func() interface{} { return new(stringRoot) }
+			case "StringUnicode":
+				newFn = func() interface{} { return new(stringRoot) }
 			case "SyntheaFhir":
 				newFn = func() interface{} { return new(syntheaRoot) }
 			}
@@ -97,6 +101,71 @@ type (
 		MinT     uint64       `json:"min_t"`
 		MaxT     uint64       `json:"max_t"`
 		MeanT    uint64       `json:"mean_t"`
+	}
+)
+
+type (
+	stringRoot struct {
+		Arabic                             string `json:"Arabic"`
+		ArabicPresentationFormsA           string `json:"Arabic Presentation Forms-A"`
+		ArabicPresentationFormsB           string `json:"Arabic Presentation Forms-B"`
+		Armenian                           string `json:"Armenian"`
+		Arrows                             string `json:"Arrows"`
+		Bengali                            string `json:"Bengali"`
+		Bopomofo                           string `json:"Bopomofo"`
+		BoxDrawing                         string `json:"Box Drawing"`
+		CJKCompatibility                   string `json:"CJK Compatibility"`
+		CJKCompatibilityForms              string `json:"CJK Compatibility Forms"`
+		CJKCompatibilityIdeographs         string `json:"CJK Compatibility Ideographs"`
+		CJKSymbolsAndPunctuation           string `json:"CJK Symbols and Punctuation"`
+		CJKUnifiedIdeographs               string `json:"CJK Unified Ideographs"`
+		CJKUnifiedIdeographsExtensionA     string `json:"CJK Unified Ideographs Extension A"`
+		CJKUnifiedIdeographsExtensionB     string `json:"CJK Unified Ideographs Extension B"`
+		Cherokee                           string `json:"Cherokee"`
+		CurrencySymbols                    string `json:"Currency Symbols"`
+		Cyrillic                           string `json:"Cyrillic"`
+		CyrillicSupplementary              string `json:"Cyrillic Supplementary"`
+		Devanagari                         string `json:"Devanagari"`
+		EnclosedAlphanumerics              string `json:"Enclosed Alphanumerics"`
+		EnclosedCJKLettersAndMonths        string `json:"Enclosed CJK Letters and Months"`
+		Ethiopic                           string `json:"Ethiopic"`
+		GeometricShapes                    string `json:"Geometric Shapes"`
+		Georgian                           string `json:"Georgian"`
+		GreekAndCoptic                     string `json:"Greek and Coptic"`
+		Gujarati                           string `json:"Gujarati"`
+		Gurmukhi                           string `json:"Gurmukhi"`
+		HangulCompatibilityJamo            string `json:"Hangul Compatibility Jamo"`
+		HangulJamo                         string `json:"Hangul Jamo"`
+		HangulSyllables                    string `json:"Hangul Syllables"`
+		Hebrew                             string `json:"Hebrew"`
+		Hiragana                           string `json:"Hiragana"`
+		IPAExtentions                      string `json:"IPA Extentions"`
+		KangxiRadicals                     string `json:"Kangxi Radicals"`
+		Katakana                           string `json:"Katakana"`
+		Khmer                              string `json:"Khmer"`
+		KhmerSymbols                       string `json:"Khmer Symbols"`
+		Latin                              string `json:"Latin"`
+		LatinExtendedAdditional            string `json:"Latin Extended Additional"`
+		Latin1Supplement                   string `json:"Latin-1 Supplement"`
+		LatinExtendedA                     string `json:"Latin-Extended A"`
+		LatinExtendedB                     string `json:"Latin-Extended B"`
+		LetterlikeSymbols                  string `json:"Letterlike Symbols"`
+		Malayalam                          string `json:"Malayalam"`
+		MathematicalAlphanumericSymbols    string `json:"Mathematical Alphanumeric Symbols"`
+		MathematicalOperators              string `json:"Mathematical Operators"`
+		MiscellaneousSymbols               string `json:"Miscellaneous Symbols"`
+		Mongolian                          string `json:"Mongolian"`
+		NumberForms                        string `json:"Number Forms"`
+		Oriya                              string `json:"Oriya"`
+		PhoneticExtensions                 string `json:"Phonetic Extensions"`
+		SupplementalArrowsB                string `json:"Supplemental Arrows-B"`
+		Syriac                             string `json:"Syriac"`
+		Tamil                              string `json:"Tamil"`
+		Thaana                             string `json:"Thaana"`
+		Thai                               string `json:"Thai"`
+		UnifiedCanadianAboriginalSyllabics string `json:"Unified Canadian Aboriginal Syllabics"`
+		YiRadicals                         string `json:"Yi Radicals"`
+		YiSyllables                        string `json:"Yi Syllables"`
 	}
 )
 
