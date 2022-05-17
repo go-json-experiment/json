9fce4ed2128718d1d4839fd6b34b942454bbf804
Joe Tsai <joetsai@digital-static.net>
2022-05-16 17:41:22 -0700

    Add example for format flags (#211)

--- example_test.go
+++ example_test.go
@@ -6,10 +6,64 @@ package json_test
 
 import (
 	"fmt"
+	"log"
+	"math"
 	"reflect"
+	"time"
 
 	"github.com/go-json-experiment/json"
 )
+
+// The "format" tag option can be used to alter the formatting of certain types.
+func Example_formatFlags() {
+	value := struct {
+		BytesBase64    []byte         `json:",format:base64"`
+		BytesHex       [8]byte        `json:",format:hex"`
+		BytesArray     []byte         `json:",format:array"`
+		FloatNonFinite float64        `json:",format:nonfinite"`
+		MapEmitNull    map[string]any `json:",format:emitnull"`
+		SliceEmitNull  []any          `json:",format:emitnull"`
+		TimeDateOnly   time.Time      `json:",format:'2006-01-02'"`
+		DurationNanos  time.Duration  `json:",format:nanos"`
+	}{
+		BytesBase64:    []byte{0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef},
+		BytesHex:       [8]byte{0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef},
+		BytesArray:     []byte{0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef},
+		FloatNonFinite: math.NaN(),
+		MapEmitNull:    nil,
+		SliceEmitNull:  nil,
+		TimeDateOnly:   time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+		DurationNanos:  time.Second + time.Millisecond + time.Microsecond + time.Nanosecond,
+	}
+
+	b, err := json.Marshal(&value)
+	if err != nil {
+		log.Fatal(err)
+	}
+	(*json.RawValue)(&b).Indent("", "\t") // indent for readability
+	fmt.Println(string(b))
+
+	// Output:
+	// {
+	// 	"BytesBase64": "ASNFZ4mrze8=",
+	// 	"BytesHex": "0123456789abcdef",
+	// 	"BytesArray": [
+	// 		1,
+	// 		35,
+	// 		69,
+	// 		103,
+	// 		137,
+	// 		171,
+	// 		205,
+	// 		239
+	// 	],
+	// 	"FloatNonFinite": "NaN",
+	// 	"MapEmitNull": null,
+	// 	"SliceEmitNull": null,
+	// 	"TimeDateOnly": "2000-01-01",
+	// 	"DurationNanos": 1001001001
+	// }
+}
 
 // In some applications, the exact precision of JSON numbers needs to be
 // preserved when unmarshaling. This can be accomplished using a type-specific
