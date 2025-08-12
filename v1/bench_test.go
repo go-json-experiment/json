025ef6036f05668821de6c085d87d353fa356b34
Joe Tsai <joetsai@digital-static.net>
2025-08-11 17:39:45 -0700

    Use types alias under goexperiment.jsonv2 and Go 1.25
    When built under both goexperiment.jsonv2 and Go 1.25,
    make the following packages aliases to the standard library:
    
    * "github.com/go-json-experiment/json/v1"       => "encoding/json"
    * "github.com/go-json-experiment/json"          => "encoding/json/v2"
    * "github.com/go-json-experiment/json/jsontext" => "encoding/json/jsontext"
    
    This ensures consistency within binaries that imports both
    "encoding/json/jsontext" and "github.com/go-json-experiment/json/jsontext"
    refer to the same implementation.
    This is especially import to ensure that references to
    jsontext.Encoder and jsontext.Decoder are the same type
    regardless of the underlying package path.

--- v1/bench_test.go
+++ v1/bench_test.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build !goexperiment.jsonv2 || !go1.25
+
 // Large data benchmark.
 // The JSON data is a summary of agl's changes in the
 // go, webkit, and chromium open source projects.
